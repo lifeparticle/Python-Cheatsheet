0e01f3533422d0c9fab7089ad68bbb7f7253285f
lifeparticle <mahbubzaman17@gmail.com>
2021-09-12 12:30:32 +1000

    init

--- scripts/web_scraper/web_scraper.py
+++ scripts/web_scraper/web_scraper.py
@@ -1,1 +1,36 @@
+from selenium import webdriver
+from urllib.parse import urljoin
 
+def navigate(driver, url, page_no):
+	driver.get(urljoin(url, str(page_no)))
+
+
+def get_data(driver, class_name):
+	return driver.find_elements_by_class_name(class_name)
+
+
+def has_data(data):
+	return len(data) > 0
+
+def parse_data(data, class_names):
+	for quote in data:
+		text = quote.find_element_by_class_name(class_names['text']).text
+		author = quote.find_element_by_class_name(class_names['author']).text
+		print("%s -- %s" %(text, author))
+
+def main():
+	driver = webdriver.Chrome()
+	url = "http://quotes.toscrape.com/page/"
+	page_no = 1
+	while True:
+		navigate(driver, url, page_no)
+		data = get_data(driver, 'quote')
+		parse_data(data, {"text": "text", "author": "author"})
+		page_no += 1
+		if (has_data(data) == False):
+			break
+	driver.close()
+
+
+if __name__ == '__main__':
+	main()
